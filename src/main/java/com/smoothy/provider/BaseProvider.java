0dd3c5949af617529730b09b13d5126b4c68547f
muxi.lc <muxi.lc@alibaba-inc.com>
2016-12-04 00:54:18 +0800

    basic info

---
+++ src/main/java/com/smoothy/provider/BaseProvider.java
@@ -0,0 +1,14 @@
+package com.smoothy.provider;
+
+import com.smoothy.service.*;
+
+/**
+ * Created by smoothy on 2016/12/4.
+ * code for free and balance  */
+public class BaseProvider {
+    DeliveryService deliveryService;
+    GarrentService garrentService;
+    ItemService itemService;
+    MarketingService marketingService;
+    PromotionService promotionService;
+}
