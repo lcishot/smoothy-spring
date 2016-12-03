d2e467abc0d033835321f0cc702cde599f99f546
muxi.lc <muxi.lc@alibaba-inc.com>
2016-12-04 00:17:57 +0800

    hello world

---
+++ src/main/java/com/smoothy/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.smoothy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2016/12/4.
+ */
+@RestController
+public class HelloController {
+    @RequestMapping(name = "hello")
+    public String hello(){
+        return "hello "+"now:"+new Date();
+    }
+}
